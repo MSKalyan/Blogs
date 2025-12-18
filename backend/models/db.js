2787cf7c8219c98dc5aae0dd0c3823720dd10156
Srinivasa kalyan <mskalyan2004@gmail.com>
2025-12-18 21:39:44 +0530

    prepare code for deployment

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,13 +1,15 @@
 // db.js - Setup the database connection with PostgreSQL
 import { Pool }  from 'pg'
+import dotenv from 'dotenv';
 
+dotenv.config();
 // Create a pool of connections to PostgreSQL
 const pool = new Pool({
-  user: 'postgres',         // Replace with your PostgreSQL username
-  host: 'localhost',             // Replace with your PostgreSQL host if needed
-  database: 'postgres',     // Replace with your PostgreSQL database name
-  password: 'dattu$diwali',     // Replace with your PostgreSQL password
-  port: 5432,                   // Default PostgreSQL port
+  user: process.env.user,         // Replace with your PostgreSQL username
+  host: process.env.host,             // Replace with your PostgreSQL host if needed
+  database: process.env.database,     // Replace with your PostgreSQL database name
+  password: process.env.password,     // Replace with your PostgreSQL password
+  port: process.env.port,                   // Default PostgreSQL port
 });
 
 // Export the pool for use in other modules
