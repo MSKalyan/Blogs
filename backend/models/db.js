867702b59ca42f47e4df580471b5bbfa7d1a72b9
MSKalyan <mskalyan2004@gmail.com>
2026-05-27 18:07:52 +0530

    cors fix

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,19 +1,36 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// console.log("DB_HOST seen by db.js =",process.env.DB_HOST);
+// delete process.env.DATABASE_URL;
+// import { Pool } from 'pg';
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: 5432,
+//   // ssl: { rejectUnauthorized: false }
+
+// });
+
+// export default pool;
+
+
+
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log("DB_HOST seen by db.js =",process.env.DB_HOST);
-delete process.env.DATABASE_URL;
-import { Pool } from 'pg';
+import pkg from 'pg';
+
+const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432,
-  // ssl: { rejectUnauthorized: false }
-
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
-
